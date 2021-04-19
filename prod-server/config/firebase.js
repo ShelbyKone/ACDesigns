ecebb0802e48a7f30f1bedf740e69ccb91b8d96a
Shelby Kone <shelbykoneis@gmail.com>
2021-04-19 13:02:21 -0600

    updated firebase func

--- prod-server/config/firebase.js
+++ prod-server/config/firebase.js
@@ -10,11 +10,13 @@ var admin = _interopRequireWildcard(_firebaseAdmin);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
+console.log(process.env.PRIVATE_KEY);
+
 var serviceAccountObject = {
   type: "service_account",
   project_id: "acdesigns-402c0",
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\n/g, '\n'),
+  private_key: process.env.PRIVATE_KEY,
   client_email: "firebase-adminsdk-iflxs@acdesigns-402c0.iam.gserviceaccount.com",
   client_id: "117127454562999419222",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
